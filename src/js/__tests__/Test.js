30f1feca941206abdabc8461c47a95f2cd3bafeb
Alenananana <85957000+Alenananana@users.noreply.github.com>
2022-05-24 19:25:59 +0500

    1

---
+++ src/js/__tests__/Test.js
@@ -0,0 +1,43 @@
+import orderByProps from '../sorting';
+
+const obj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+test('Порядок элементов', () => {
+  const expected = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+  expect(orderByProps(obj, ['name', 'level']))
+    .toEqual(expected);
+});
+
+test('Возврат результата', () => {
+  expect(orderByProps(obj))
+    .toEqual([
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+    ]);
+});
+
+test('Соответствие порядка элементов', () => {
+  expect(orderByProps(obj, ['level', 'defence', 'health', 'name', 'attack']))
+    .toEqual([
+      { key: 'level', value: 2 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'name', value: 'мечник' },
+      { key: 'attack', value: 80 },
+    ]);
+});
